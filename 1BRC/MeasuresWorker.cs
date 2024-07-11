b86d950c58522dbd4aee4c876b732b765a2a8def
Youssef.Hamaoui <Youssef.Hamaoui@solera.com>
2024-07-11 19:59:41 +0200

    optimaze Buffer size to 128*128: 00:02:42.000

---
+++ 1BRC/MeasuresWorker.cs
@@ -0,0 +1,75 @@
+using System.Collections.Concurrent;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace _1BRC;
+
+public class MeasuresWorker
+{
+    // private readonly int _bufferLength;
+    // [Params(1024,128*128,128*256,256*256,256*512,512*512,1024*1024)]
+    public int _bufferLength { get; set; }
+    
+    private readonly string _measurementFile = "./measurements-1_000_000-sample.txt";
+    // ConcurrentDictionary<string, Measures> measures = new ();
+
+    public MeasuresWorker(string measurementFile, int bufferLength = 128*128)
+    {
+        _measurementFile = measurementFile;
+        _bufferLength = bufferLength;
+    }
+    
+    // [Benchmark]
+    public async Task Process(ConcurrentDictionary<string, Measures> measures)
+    {
+        await foreach (string s in ReadFile(_measurementFile))
+        {
+            (string name, decimal measure) = ProcessLines(s);
+            measures.AddOrUpdate(name,
+                _ => new Measures(measure, (long)measure, measure, 1),
+                (_, m) =>
+                {
+                    var min = measure < m.Min ? measure : m.Min;
+                    var max = measure > m.Max ? measure : m.Max;
+                    var sum = m.Sum + measure;
+                    var count = m.Count + 1;
+                    return new Measures(min, (long)sum, max, count);
+                });
+        }
+    }
+    
+    async IAsyncEnumerable<string> ReadFile(string filePath)
+    {
+        var file = File.OpenRead(filePath);
+        byte[] buffer = new byte[_bufferLength];
+        Memory<byte> copy = Memory<byte>.Empty;
+    
+        string pending = String.Empty;
+        while (await file.ReadAsync(buffer) > 0)
+        {
+            copy = new Memory<byte>(buffer);
+        
+            var s = pending + Encoding.UTF8.GetString(copy.Span);
+            var lastIndexOf = s.LastIndexOf('\n');
+            lastIndexOf++;
+            pending = s[lastIndexOf..];
+            yield return s[..lastIndexOf];
+        }
+    
+    }
+
+    (string, decimal) ProcessLines(string s)
+    {
+        var lines = s.Split('\n');
+        foreach (var line in lines)
+        {
+            var words = line.Split(";");
+            decimal.TryParse(words[1], out var measure);
+            return (words[0], measure);
+        }
+
+        return (String.Empty, 0);
+    }
+
+
+}
--- 1BRC/Program.cs
+++ 1BRC/Program.cs
@@ -1,84 +1,24 @@
 ﻿using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Text;
-using _1BRC;
+using _1BRC;using BenchmarkDotNet.Running;
 
-// string measurementFile = "./measurements.txt";
-string measurementFile = "./measurements-1_000_000-sample.txt";
+string measurementFile = "./measurements.txt";
+// string measurementFile = "./measurements-1_000_000-sample.txt";
 //
 // new CreateMeasurements().CreateMesurementsFile(measurementFile, 1_000_000_000);
 // return;
 
 ConcurrentDictionary<string, Measures> measures = new ConcurrentDictionary<string, Measures>();
 
+// BenchmarkRunner.Run<MeasuresWorker>();
+// return;
 
 var sw = new Stopwatch();
+var worker = new MeasuresWorker(measurementFile);
+
 sw.Start();
-
-
-async IAsyncEnumerable<string> ReadFile(string filePath)
-{
-    var file = File.OpenRead(measurementFile);
-    byte[] buffer = new byte[1024*1024];
-    Memory<byte> copy = Memory<byte>.Empty;
-    
-    string pending = String.Empty;
-    while (await file.ReadAsync(buffer) > 0)
-    {
-        copy = new Memory<byte>(buffer);
-        
-        var s = pending + Encoding.UTF8.GetString(copy.Span);
-        var lastIndexOf = s.LastIndexOf('\n');
-        lastIndexOf++;
-        pending = s[lastIndexOf..];
-        yield return s[..lastIndexOf];
-    }
-    
-}
-
-(string, decimal) ProcessLines(string s)
-{
-    var lines = s.Split('\n');
-    foreach (var line in lines)
-    {
-        var words = line.Split(";");
-        decimal.TryParse(words[1], out var measure);
-        return (words[0], measure);
-    }
-
-    return (String.Empty, 0);
-}
-
-
-await foreach (string s in ReadFile(measurementFile))
-{
-    (string name, decimal measure) = ProcessLines(s);
-    measures.AddOrUpdate(name,
-        _ => new Measures(measure, (long)measure, measure, 1),
-        (_, m) =>
-        {
-            var min = measure < m.Min ? measure : m.Min;
-            var max = measure > m.Max ? measure : m.Max;
-            var sum = m.Sum + measure;
-            var count = m.Count + 1;
-            return new Measures(min, (long)sum, max, count);
-        });
-}
-
-// await Parallel.ForEachAsync(ReadFile(measurementFile), async (s, _) =>
-// {
-//     (string name, decimal measure) = ProcessLines(s);
-//     measures.AddOrUpdate(name,
-//         _ => new Measures(measure, (long)measure, measure, 1),
-//         (_, m) =>
-//         {
-//             var min = measure < m.Min ? measure : m.Min;
-//             var max = measure > m.Max ? measure : m.Max;
-//             var sum = m.Sum + measure;
-//             var count = m.Count + 1;
-//             return new Measures(min, (long)sum, max, count);
-//         });
-// });
+await worker.Process(measures);
 
 
 foreach (var measure in measures)
